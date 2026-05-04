507e80203bd1526e297675b125e6d95239e0c0f3
luigi beraldi <luiggiberaldi94@gmail.com>
2026-05-03 23:59:50 -0400

    fix(ui): agregar padding inferior al modal transportista en venta rapida para que placa batea no quede tapada

--- src/views/VentaRapidaView.jsx
+++ src/views/VentaRapidaView.jsx
@@ -1435,7 +1435,7 @@ function Step2Pago({
                   <X size={18} />
                 </button>
               </div>
-              <div className="p-4">
+              <div className="p-4 pb-24">
                 {transpError && (
                   <div className="bg-red-50 border border-red-200 rounded-xl px-3 py-2 text-sm text-red-700 mb-3">
                     {transpError}
